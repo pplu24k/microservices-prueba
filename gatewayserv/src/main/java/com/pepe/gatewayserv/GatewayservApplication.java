8c145f45e450beda64a50711243f028e1cb7fa72
pplu24k <joseluespp@gmail.com>
2023-05-31 09:12:12 +0200

    gateway

---
+++ gatewayserv/src/main/java/com/pepe/gatewayserv/GatewayservApplication.java
@@ -0,0 +1,13 @@
+package com.pepe.gatewayserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayservApplication.class, args);
+	}
+
+}
